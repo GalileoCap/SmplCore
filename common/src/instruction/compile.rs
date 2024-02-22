ff2014f2f73236656f8756792e2e81bfbb9c3726
GalileoCap <galileocapp@gmail.com>
2024-02-21 22:07:04 -0300

    OK: All Mov

--- common/src/instruction/compile.rs
+++ common/src/instruction/compile.rs
@@ -8,10 +8,16 @@ impl Instruction {
         match self {
             Nop => vec![self.opcode(), 0x00],
 
-            MovI2R(value, dest) | MovI2RP(value, dest)
+            MovI2IP(src, dest) | MovIP2IP(src, dest)
+                => vec![self.opcode(), 0x00, src.get_byte(0), src.get_byte(1), dest.get_byte(0), dest.get_byte(1)],
+
+            MovI2R(value, dest) | MovI2RP(value, dest) | MovIP2R(value, dest) | MovIP2RP(value, dest)
                 => vec![self.opcode(), dest.as_dest(), value.get_byte(0), value.get_byte(1)],
 
-            MovR2R(src, dest) | MovR2RP(src, dest) | MovRP2R(src, dest)
+            MovR2IP(src, dest) | MovRP2IP(src, dest)
+                => vec![self.opcode(), src.as_src(), dest.get_byte(0), dest.get_byte(1)],
+
+            MovR2R(src, dest) | MovR2RP(src, dest) | MovRP2R(src, dest) | MovRP2RP(src, dest)
                 => vec![self.opcode(), src.as_src_with(dest)],
         }
     }
@@ -31,9 +37,16 @@ impl Instruction {
             Nop => 0x00,
             MovI2R(value, _) => case!(value, 0x01),
             MovI2RP(value, _) => case!(value, 0x03),
-            MovR2R(src, _) => case!(src, 0x05),
-            MovR2RP(src, _) => case!(src, 0x07),
-            MovRP2R(_, dest) => case!(dest, 0x09),
+            MovI2IP(value, _) => case!(value, 0x05),
+            MovIP2R(_, dest) => case!(dest, 0x07),
+            MovIP2RP(_, _) => 0x09,
+            MovIP2IP(_, _) => 0x0A,
+            MovR2R(src, _) => case!(src, 0x0B),
+            MovR2RP(src, _) => case!(src, 0x0C),
+            MovR2IP(src, _) => case!(src, 0x0E),
+            MovRP2R(_, dest) => case!(dest, 0x10),
+            MovRP2RP(_, _) => 0x12,
+            MovRP2IP(_, _) => 0x13,
         }
     }
 }
@@ -87,18 +100,49 @@ mod test {
         Immediate::word(0x600D), Register::R0, [0x04, 0x00, 0x0D, 0x60]
     );
     test_case!(
+        movi2ip,
+        Immediate::byte(0x60), Immediate::word(0xF337), [0x05, 0x00, 0x60, 0x00, 0x37, 0xF3];
+        Immediate::word(0x600D), Immediate::word(0xF337), [0x06, 0x00, 0x0D, 0x60, 0x37, 0xF3]
+    );
+    test_case!(
+        movip2r,
+        Immediate::word(0x600D), Register::Rb0, [0x07, 0x00, 0x0D, 0x60];
+        Immediate::word(0x600D), Register::R0, [0x08, 0x00, 0x0D, 0x60]
+    );
+    test_case!(
+        movip2rp,
+        Immediate::word(0x600D), Register::R0, [0x09, 0x00, 0x0D, 0x60]
+    );
+    test_case!(
+        movip2ip,
+        Immediate::word(0x600D), Immediate::word(0xF337), [0x0A, 0x00, 0x0D, 0x60, 0x37, 0xF3]
+    );
+    test_case!(
         movr2r,
-        Register::Rb0, Register::Rb1, [0x05, 0x10];
-        Register::R0, Register::R1, [0x06, 0x10]
+        Register::Rb0, Register::Rb1, [0x0B, 0x10];
+        Register::R0, Register::R1, [0x0C, 0x10]
     );
     test_case!(
         movr2rp,
-        Register::Rb0, Register::R1, [0x07, 0x10];
-        Register::R0, Register::R1, [0x08, 0x10]
+        Register::Rb0, Register::R1, [0x0C, 0x10];
+        Register::R0, Register::R1, [0x0D, 0x10]
+    );
+    test_case!(
+        movr2ip,
+        Register::Rb0, Immediate::word(0x600D), [0x0E, 0x00, 0x0D, 0x60];
+        Register::R0, Immediate::word(0x600D), [0x0F, 0x00, 0x0D, 0x60]
     );
     test_case!(
         movrp2r,
-        Register::R0, Register::Rb1, [0x09, 0x10];
-        Register::R0, Register::R1, [0x0A, 0x10]
+        Register::R0, Register::Rb1, [0x10, 0x10];
+        Register::R0, Register::R1, [0x11, 0x10]
+    );
+    test_case!(
+        movrp2rp,
+        Register::R0, Register::R1, [0x12, 0x10]
+    );
+    test_case!(
+        movrp2ip,
+        Register::R0, Immediate::word(0x600D), [0x13, 0x00, 0x0D, 0x60]
     );
 }
--- common/src/instruction/mod.rs
+++ common/src/instruction/mod.rs
@@ -6,9 +6,16 @@ pub enum Instruction {
     Nop,
     MovI2R(Immediate, Register),
     MovI2RP(Immediate, Register),
+    MovI2IP(Immediate, Immediate),
+    MovIP2R(Immediate, Register),
+    MovIP2RP(Immediate, Register),
+    MovIP2IP(Immediate, Immediate),
     MovR2R(Register, Register),
     MovR2RP(Register, Register),
+    MovR2IP(Register, Immediate),
     MovRP2R(Register, Register),
+    MovRP2RP(Register, Register),
+    MovRP2IP(Register, Immediate),
 }
 
 macro_rules! instruction_constructor {
@@ -38,9 +45,16 @@ impl Instruction {
 
     instruction_constructor!(movi2r, MovI2R, Immediate, Register);
     instruction_constructor!(movi2rp, MovI2RP, Immediate, Register);
+    instruction_constructor!(movi2ip, MovI2IP, Immediate, Immediate);
+    instruction_constructor!(movip2r, MovIP2R, Immediate, Register);
+    instruction_constructor!(movip2rp, MovIP2RP, Immediate, Register);
+    instruction_constructor!(movip2ip, MovIP2IP, Immediate, Immediate);
     instruction_constructor!(movr2r, MovR2R, Register, Register);
     instruction_constructor!(movr2rp, MovR2RP, Register, Register);
+    instruction_constructor!(movr2ip, MovR2IP, Register, Immediate);
     instruction_constructor!(movrp2r, MovRP2R, Register, Register);
+    instruction_constructor!(movrp2rp, MovRP2RP, Register, Register);
+    instruction_constructor!(movrp2ip, MovRP2IP, Register, Immediate);
 
     fn check_valid(self) -> Result<Self> {
         if self.is_valid() {
@@ -57,9 +71,16 @@ impl Instruction {
 
             MovI2R(src, dest) => src.width() == dest.width(),
             MovI2RP(_, dest) => dest.width() == Width::Word,
+            MovI2IP(_, dest) => dest.width() == Width::Word,
+            MovIP2R(src, _) => src.width() == Width::Word,
+            MovIP2RP(src, dest) => src.width() == Width::Word && dest.width() == Width::Word,
+            MovIP2IP(src, dest) => src.width() == Width::Word && dest.width() == Width::Word,
             MovR2R(src, dest) => src.width() == dest.width(),
             MovR2RP(_, dest) => dest.width() == Width::Word,
+            MovR2IP(_, dest) => dest.width() == Width::Word,
             MovRP2R(src, _) => src.width() == Width::Word,
+            MovRP2RP(src, dest) => src.width() == Width::Word && dest.width() == Width::Word,
+            MovRP2IP(src, dest) => src.width() == Width::Word && dest.width() == Width::Word,
         }
     }
 }
@@ -111,6 +132,21 @@ mod test {
         Immediate::word(0x600D), Register::Rb0
     );
     test_case!(
+        movi2ip,
+        Immediate::byte(0x60), Immediate::word(0xF337),
+        Immediate::word(0x600D), Immediate::word(0xF337)
+        ;
+        Immediate::byte(0x60), Immediate::byte(0xF3),
+        Immediate::word(0x600D), Immediate::byte(0xF3)
+    );
+    test_case!(
+        movip2ip,
+        Immediate::word(0x600D), Immediate::word(0xF337)
+        ;
+        Immediate::byte(0x60), Immediate::word(0xF337),
+        Immediate::word(0x600D), Immediate::byte(0xF3)
+    );
+    test_case!(
         movr2r,
         Register::Rb0, Register::Rb1,
         Register::R0, Register::R1
@@ -127,8 +163,23 @@ mod test {
         Register::R0, Register::Rb1
     );
     test_case!(
+        movr2ip,
+        Register::Rb0, Immediate::word(0x600D),
+        Register::R0, Immediate::word(0x600D)
+        ;
+        Register::Rb0, Immediate::byte(0x60),
+        Register::R0, Immediate::byte(0x60)
+    );
+    test_case!(
         movrp2r,
         Register::R0, Register::Rb1,
+        Register::R0, Register::R1
+        ;
+        Register::Rb0, Register::R1,
+        Register::Rb0, Register::Rb1
+    );
+    test_case!(
+        movrp2rp,
         Register::R0, Register::R1
         ;
         Register::Rb0, Register::R1,
