193d219fb4ee79cf1dab2ab9f4f9242a44af39c4
GalileoCap <galileocapp@gmail.com>
2024-02-26 21:27:11 -0300

    FIX(common): Overlapping opcodes

--- common/src/instruction/compile.rs
+++ common/src/instruction/compile.rs
@@ -42,11 +42,11 @@ impl Instruction {
             MovIP2RP(_, _) => 0x09,
             MovIP2IP(_, _) => 0x0A,
             MovR2R(src, _) => case!(src, 0x0B),
-            MovR2RP(src, _) => case!(src, 0x0C),
-            MovR2IP(src, _) => case!(src, 0x0E),
-            MovRP2R(_, dest) => case!(dest, 0x10),
-            MovRP2RP(_, _) => 0x12,
-            MovRP2IP(_, _) => 0x13,
+            MovR2RP(src, _) => case!(src, 0x0D),
+            MovR2IP(src, _) => case!(src, 0x0F),
+            MovRP2R(_, dest) => case!(dest, 0x11),
+            MovRP2RP(_, _) => 0x13,
+            MovRP2IP(_, _) => 0x14,
         }
     }
 }
@@ -127,25 +127,25 @@ mod test {
     );
     test_case!(
         movr2rp,
-        Register::Rb0, Register::R1, [0x0C, 0x10];
-        Register::R0, Register::R1, [0x0D, 0x10]
+        Register::Rb0, Register::R1, [0x0D, 0x10];
+        Register::R0, Register::R1, [0x0E, 0x10]
     );
     test_case!(
         movr2ip,
-        Register::Rb0, Immediate::word(0x600D), [0x0E, 0x00, 0x0D, 0x60];
-        Register::R0, Immediate::word(0x600D), [0x0F, 0x00, 0x0D, 0x60]
+        Register::Rb0, Immediate::word(0x600D), [0x0F, 0x00, 0x0D, 0x60];
+        Register::R0, Immediate::word(0x600D), [0x10, 0x00, 0x0D, 0x60]
     );
     test_case!(
         movrp2r,
-        Register::R0, Register::Rb1, [0x10, 0x10];
-        Register::R0, Register::R1, [0x11, 0x10]
-    );
-    test_case!(
-        movrp2rp,
+        Register::R0, Register::Rb1, [0x11, 0x10];
         Register::R0, Register::R1, [0x12, 0x10]
     );
     test_case!(
+        movrp2rp,
+        Register::R0, Register::R1, [0x13, 0x10]
+    );
+    test_case!(
         movrp2ip,
-        Register::R0, Immediate::word(0x600D), [0x13, 0x00, 0x0D, 0x60]
+        Register::R0, Immediate::word(0x600D), [0x14, 0x00, 0x0D, 0x60]
     );
 }
