d40623980f98f8a7663532b2486ec7ffa891ebd0
GalileoCap <galileocapp@gmail.com>
2024-02-26 21:21:09 -0300

    FEAT(common): Decompile MovIP2X

--- common/src/instruction/decompile.rs
+++ common/src/instruction/decompile.rs
@@ -12,6 +12,14 @@ impl Instruction {
             0x00 => Self::nop(),
             0x01 => Self::decompile_movi2r(Width::Byte, *regs, bytes),
             0x02 => Self::decompile_movi2r(Width::Word, *regs, bytes),
+            0x03 => Self::decompile_movi2rp(Width::Byte, *regs, bytes),
+            0x04 => Self::decompile_movi2rp(Width::Word, *regs, bytes),
+            0x05 => Self::decompile_movi2ip(Width::Byte, bytes),
+            0x06 => Self::decompile_movi2ip(Width::Word, bytes),
+            0x07 => Self::decompile_movip2r(Width::Byte, *regs, bytes),
+            0x08 => Self::decompile_movip2r(Width::Word, *regs, bytes),
+            0x09 => Self::decompile_movip2rp(*regs, bytes),
+            0x0A => Self::decompile_movip2ip(bytes),
             _ => Err(Error::NoSuchOpcode(*opcode)),
         }
     }
@@ -23,5 +31,58 @@ impl Instruction {
         let value = Immediate::new(width, value)?;
         let dest = Register::from_dest(width, regs);
         Instruction::movi2r(value, dest)
+    }
+
+    fn decompile_movi2rp(width : Width, regs : u8, bytes : &[u8]) -> Result<Self> {
+        let Some(value_lower) = bytes.get(2) else { return Err(Error::NoValue(0)) };
+        let Some(value_higher) = bytes.get(3) else { return Err(Error::NoValue(1)) };
+        let value = (*value_lower as u64) | ((*value_higher as u64) << 8);
+        let value = Immediate::new(width, value)?;
+        let dest = Register::from_dest(Width::Word, regs);
+        Instruction::movi2rp(value, dest)
+    }
+
+    fn decompile_movi2ip(width : Width, bytes : &[u8]) -> Result<Self> {
+        let Some(value_lower) = bytes.get(2) else { return Err(Error::NoValue(0)) };
+        let Some(value_higher) = bytes.get(3) else { return Err(Error::NoValue(1)) };
+        let value = (*value_lower as u64) | ((*value_higher as u64) << 8);
+        let value = Immediate::new(width, value)?;
+
+        let Some(dest_lower) = bytes.get(4) else { return Err(Error::NoValue(0)) };
+        let Some(dest_higher) = bytes.get(5) else { return Err(Error::NoValue(1)) };
+        let dest = (*dest_lower as u64) | ((*dest_higher as u64) << 8);
+        let dest = Immediate::new(Width::Word, dest)?;
+        Instruction::movi2ip(value, dest)
+    }
+
+    fn decompile_movip2r(width : Width, regs : u8, bytes : &[u8]) -> Result<Self> {
+        let Some(value_lower) = bytes.get(2) else { return Err(Error::NoValue(0)) };
+        let Some(value_higher) = bytes.get(3) else { return Err(Error::NoValue(1)) };
+        let value = (*value_lower as u64) | ((*value_higher as u64) << 8);
+        let value = Immediate::new(Width::Word, value)?;
+        let dest = Register::from_dest(width, regs);
+        Instruction::movip2r(value, dest)
+    }
+
+    fn decompile_movip2rp(regs : u8, bytes : &[u8]) -> Result<Self> {
+        let Some(value_lower) = bytes.get(2) else { return Err(Error::NoValue(0)) };
+        let Some(value_higher) = bytes.get(3) else { return Err(Error::NoValue(1)) };
+        let value = (*value_lower as u64) | ((*value_higher as u64) << 8);
+        let value = Immediate::new(Width::Word, value)?;
+        let dest = Register::from_dest(Width::Word, regs);
+        Instruction::movip2rp(value, dest)
+    }
+
+    fn decompile_movip2ip(bytes : &[u8]) -> Result<Self> {
+        let Some(value_lower) = bytes.get(2) else { return Err(Error::NoValue(0)) };
+        let Some(value_higher) = bytes.get(3) else { return Err(Error::NoValue(1)) };
+        let value = (*value_lower as u64) | ((*value_higher as u64) << 8);
+        let value = Immediate::new(Width::Word, value)?;
+
+        let Some(dest_lower) = bytes.get(4) else { return Err(Error::NoValue(0)) };
+        let Some(dest_higher) = bytes.get(5) else { return Err(Error::NoValue(1)) };
+        let dest = (*dest_lower as u64) | ((*dest_higher as u64) << 8);
+        let dest = Immediate::new(Width::Word, dest)?;
+        Instruction::movip2ip(value, dest)
     }
 }
